e3ce4d676fd2bf770d5f78207721decd7fd51615
AliAksoyy <alifeyza4017@gmail.com>
2024-09-24 13:14:29 +0300

    added

--- main.js
+++ main.js
@@ -1,12 +1,50 @@
 let localStream;
 let remoteStream;
+let peerConnection;
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+};
 
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true,
+    audio: false,
   });
+
   document.getElementById("user-1").srcObject = localStream;
+  createOffer();
+};
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection(servers);
+
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
+
+  localStream.getTracks().forEach((element) => {
+    peerConnection.addTrack(element, localStream);
+  });
+
+  peerConnection.ontrack = (e) => {
+    e.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack();
+    });
+  };
+
+  peerConnection.onicecandidate = async (e) => {
+    if (e.candidate) {
+      console.log("New ICE candidate:", e.candidate);
+    }
+  };
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  console.log("Offer:", offer);
 };
 
 init();
