96ac6bd08a52f052cb0b76bb83d5c98632acd52b
AliAksoyy <alifeyza4017@gmail.com>
2024-09-24 12:20:34 +0300

    added

---
+++ main.js
@@ -0,0 +1,12 @@
+let localStream;
+let remoteStream;
+
+let init = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById("user-1").srcObject = localStream;
+};
+
+init();
